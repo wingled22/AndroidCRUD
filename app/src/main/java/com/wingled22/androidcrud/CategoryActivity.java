d7a0e29543014f9ffa876b3b2a15b124b567f83d
wingled22 <pelayowindel@gmail.com>
2022-01-25 15:51:52 +0800

    created CategoryActivity

---
+++ app/src/main/java/com/wingled22/androidcrud/CategoryActivity.java
@@ -0,0 +1,19 @@
+package com.wingled22.androidcrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        // Get the Intent that started this activity and extract the string
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(intent.getStringExtra("data"));
+    }
+}
--- app/src/main/java/com/wingled22/androidcrud/MainActivity.java
+++ app/src/main/java/com/wingled22/androidcrud/MainActivity.java
@@ -2,13 +2,37 @@ package com.wingled22.androidcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button categoryButton ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        categoryButton = findViewById(R.id.categoryButton);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.categoryButton:
+//
+
+        }
+    }
+
+    public void categoryClicked(View view) {
+//        Toast toast = Toast.makeText(MainActivity.this,"category button is clicked", Toast.LENGTH_SHORT );
+//        toast.show();
+        Intent intent = new Intent(this, CategoryActivity.class);
+        startActivity(intent);
     }
 }
