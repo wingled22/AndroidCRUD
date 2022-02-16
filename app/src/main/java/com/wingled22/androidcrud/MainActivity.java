7d4b10c60491810a8e6be7a963dc6ac089c845f7
Winson404 <sonerwin8@gmail.com>
2022-02-16 15:27:20 +0800

    giusab ni nako

--- app/src/main/java/com/wingled22/androidcrud/MainActivity.java
+++ app/src/main/java/com/wingled22/androidcrud/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button categoryButton ;
+    Button productButton ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +17,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         categoryButton = findViewById(R.id.categoryButton);
+        productButton = findViewById(R.id.productButton);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.categoryButton:
+            case R.id.productButton:
                 //
         }
     }
@@ -30,6 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        Toast toast = Toast.makeText(MainActivity.this,"category button is clicked", Toast.LENGTH_SHORT );
 //        toast.show();
         Intent intent = new Intent(this, CategoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void productClicked(View view) {
+//        Toast toast = Toast.makeText(MainActivity.this,"category button is clicked", Toast.LENGTH_SHORT );
+//        toast.show();
+        Intent intent = new Intent(this, ProductActivity.class);
         startActivity(intent);
     }
 }
