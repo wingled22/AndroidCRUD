e6e857031f055249aa48985568c0725ef012370f
abelloif <abelloif3@gmail.com>
2022-02-16 15:59:45 +0800

    Giusab ni If4

--- app/src/main/java/com/wingled22/androidcrud/MainActivity.java
+++ app/src/main/java/com/wingled22/androidcrud/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         categoryButton = findViewById(R.id.categoryButton);
-        productButton = findViewById(R.id.productButton);
         fruitsButton = findViewById(R.id.fruitsButton);
 
     }
